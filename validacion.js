1d55bb05ca998efe9430c8fa3e4a8d1f75e51cb6
Luciana-castro-arias <luciana.castroarias@gmail.com>
2024-05-22 16:04:50 -0300

    todo listo creo

---
+++ validacion.js
@@ -0,0 +1,11 @@
+
+const validacionFormulario = (my_formulario) => {
+    const listaInputs = document.querySelectorAll(`${my_formulario} [data-validate]`);
+    console.log('listaInputs :>> ', listaInputs);
+}
+
+const enviarFormulario = () => {
+	event.preventDefault();
+	validacionFormulario(`#my_formulario`);
+}
+
